c04100eb1d8605ed1e1da2cef814b8e0d5aee797
Adrian Dupree <musicguitar48@gmail.com>
2013-12-08 13:10:20 -0500

    Accounted for undirected graphs in implementation

--- shortestpaths.py
+++ shortestpaths.py
@@ -39,57 +39,68 @@ class edge:
         self.v = v
         self.weight = weight
 
+directed = False
 with open(fileName, "r") as f:
     line = f.readline()
     if line[0] == "D":
-        Q = []
-        S = []
-        edges = []
-        for line in f:
-            lineList = line.split() #lineList contains [u, v, w]
-            edgeObj = edge(lineList[0], lineList[1], lineList[2])
-            vertexU = None
-            vertexV = None
+        directed = True
+    Q = []
+    S = []
+    edges = []
+    for line in f:
+        lineList = line.split() #lineList contains [u, v, w]
+        edgeObj = edge(lineList[0], lineList[1], int(lineList[2]))
+        vertexU = None
+        vertexV = None
             
-            if lineList[0] == "s" or lineList[0] == "S":
-                vertexU = vertex(lineList[0], 0, None)
-            else:
-                vertexU = vertex(lineList[0], -1, None)
+        if lineList[0] == "s" or lineList[0] == "S":
+            vertexU = vertex(lineList[0], 0, None)
+        else:
+            vertexU = vertex(lineList[0], -1, None)
                 
-            if vertexU not in Q:
-                Q.append(vertexU)
+        if vertexU not in Q:
+            Q.append(vertexU)
                 
-            if lineList[1] == "s" or lineList[1] == "S":
-                vertexV = vertex(lineList[1], 0, None)
-            else:
-                vertexV = vertex(lineList[1], -1, None)
+        if lineList[1] == "s" or lineList[1] == "S":
+            vertexV = vertex(lineList[1], 0, None)
+        else:
+            vertexV = vertex(lineList[1], -1, None)
             
-            if vertexV not in Q:
-                Q.append(vertexV)
-            edgeObj = edge(vertexU, vertexV, int(lineList[2]))
-            edges.append(edgeObj)
-                
-        while Q != []:
-            u = min(Q)
-            print(u.name)
-            Q.remove(u)
-            S.append(u)
-            for edge in edges: 
-                if edge.u == u:
-                    vDist = None
-                    if edge.v in Q:
-                        vIndex = Q.index(edge.v)
-                        vDist = Q[vIndex].dist
-                        if vDist == -1 or (vDist > (u.dist + int(edge.weight))): #Relax
-                            vIndex = Q.index(edge.v)
-                            Q[vIndex].dist = u.dist + int(edge.weight)
-                            Q[vIndex].pred = u
+        if vertexV not in Q:
+            Q.append(vertexV)
+        edgeObj = edge(vertexU, vertexV, int(lineList[2]))
+        edges.append(edgeObj)
+        
+    while Q != []:
+        u = min(Q)
+        print(u.name)
+        Q.remove(u)
+        S.append(u)
+        for edge in edges: 
+            if edge.u == u:
+                if edge.v in Q:
+                    vIndex = Q.index(edge.v)
+                    vDist = Q[vIndex].dist
+                    if vDist == -1 or (vDist > (u.dist + int(edge.weight))): #Relax
+                        Q[vIndex].dist = u.dist + int(edge.weight)
+                        Q[vIndex].pred = u
+        if directed == False:
+            for edge in edges:
+                if edge.v == u:
+                    x = u #x is denoted as edge (x, u) 
+                    if edge.u in Q: 
+                        uIndex = Q.index(edge.u)
+                        uDist = Q[uIndex].dist
+                        if xDist == -1 or (uDist > (x.dist + int(edge.weight))): # Relax
+                            Q[uIndex].dist = x.dist + int(edge.weight)
+                            Q[uIndex].pred = x
+                            
 
-        for v in S:
-            print("v={0} and distance={1}".format(v.name, v.dist))
+    for v in S:
+        print("v={0} and distance={1}".format(v.name, v.dist))
 
-    else:
-        print("This is an undirected graph")
+    #else:
+        #print("This is an undirected graph")
 
     
 
